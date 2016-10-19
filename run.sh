bdea956efa9d4246bc3a738c2ad055913c5d21f0
isinstance <super_big_hero@sina.com>
2016-10-19 16:57:53 +0800

    Update run.sh
    许多kali的系统的wlan0名字并不是wlan0，所以pull这个也是为了其他人看的懂一点

--- run.sh
+++ run.sh
@@ -1,8 +1,16 @@
 echo 1 > /proc/sys/net/ipv4/ip_forward
 airmon-ng check kill
+
+# You should first run the 'ifconfig' command to say what you wlan' name
+# And in many Kali system, wlan's name is wlan0mon
+# airmon-ng start wlan0mon
 airmon-ng start wlan0
 
+# If you wlan's name is wlan0mon
+# You must use this command below
+# nohup airbase-ng -P -C 30 -v wlan0mon &
 nohup airbase-ng -P -C 30 -v mon0 &
+
 ifconfig at0 up #启动interface
 ifconfig at0 192.168.3.1 netmask 255.255.255.0 #分配IP和掩码
 route add -net 192.168.3.0 netmask 255.255.255.0 gw 192.168.3.1 #增加路由项，统一由192.168.3.1(at0)来传输数据。
